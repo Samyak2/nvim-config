de11396df386ea68e58e96043261a0477e53a924
Samyak S Sarnayak <samyak201@gmail.com>
2021-05-18 18:48:34 +0530

    enable extra flags in go-ls

--- lua/lsp/go-ls.lua
+++ lua/lsp/go-ls.lua
@@ -1,6 +1,7 @@
 require'lspconfig'.gopls.setup{
-    settings = {gopls = {analyses = {unusedparams = true, fieldalignment = true},
-                staticcheck = true}},
+    settings = {gopls = {analyses = {unusedparams = true, fieldalignment = true, nilness = true},
+                         staticcheck = true,
+                         usePlaceholders = true}},
     init_options = {usePlaceholders = true, completeUnimported = true},
     on_attach = require'lsp'.common_on_attach
 }
