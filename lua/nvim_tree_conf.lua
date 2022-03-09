7cddb3182e7b6fc0208305ae73ab4383b2084b43
Samyak S Sarnayak <samyak201@gmail.com>
2022-03-09 23:10:07 +0530

    update nvim tree conf

--- lua/nvim_tree_conf.lua
+++ lua/nvim_tree_conf.lua
@@ -1,7 +1,15 @@
 require'nvim-tree'.setup {
     auto_close = true,
+    auto_reload_on_write = true,
     update_focused_file = {
         enable      = true
+    },
+    diagnostics = {
+        enable = false,
+    },
+    actions = {
+        open_file = {
+            quit_on_open = true,
+        }
     }
 }
-vim.g.nvim_tree_quit_on_open = 1
