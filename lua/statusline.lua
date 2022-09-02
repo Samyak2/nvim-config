964a6b7a49487cece7d64f02253df7a025971de9
Samyak S Sarnayak <samyak201@gmail.com>
2022-09-03 00:33:34 +0530

    fix diff colors

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -308,21 +308,21 @@ local Git = {
             local count = self.status_dict.added or 0
             return count > 0 and ("+" .. count)
         end,
-        hl = { fg = utils.get_highlight("DiffAdd").fg },
+        hl = { fg = "green" },
     },
     {
         provider = function(self)
             local count = self.status_dict.removed or 0
             return count > 0 and ("-" .. count)
         end,
-        hl = { fg = utils.get_highlight("DiffDelete").fg },
+        hl = { fg = "red" },
     },
     {
         provider = function(self)
             local count = self.status_dict.changed or 0
             return count > 0 and ("~" .. count)
         end,
-        hl = { fg = utils.get_highlight("DiffChange").fg },
+        hl = { fg = colors.gray },
     },
     {
         condition = function(self)
