6fa1dce67b9a446c6a1193e15657df13cf836d31
Samyak Sarnayak <samyak201@gmail.com>
2022-10-23 18:55:28 +0530

    fix cmp_nvim_lsp breaking change

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -66,9 +66,7 @@ function ret.common_on_attach(client, bufnr)
 end
 
 ret.common_opts = {
-    capabilities = require('cmp_nvim_lsp').update_capabilities(
-        vim.lsp.protocol.make_client_capabilities()
-    ),
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
     on_attach = ret.common_on_attach,
 }
 
