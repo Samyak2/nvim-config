cf04f81f58c683a548368f0373e0b9eafa677472
Samyak S Sarnayak <samyak201@gmail.com>
2024-01-07 16:26:17 +0530

    Keep only linter and formatter in shell.nix

--- shell.nix
+++ shell.nix
@@ -3,12 +3,7 @@
 with pkgs;
 pkgs.mkShell {
   buildInputs = [
-    gcc
-    nodejs
-    neovim
-    tree-sitter
-    (python39.withPackages (pp: with pp; [
-      pynvim
-    ]))
+    luajitPackages.luacheck
+    stylua
   ];
 }
