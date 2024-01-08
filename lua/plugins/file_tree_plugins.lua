8b97df1fdee3c29275baa069ee78df6e01293a33
Samyak S Sarnayak <samyak201@gmail.com>
2024-01-09 01:35:05 +0530

    add oil git status

--- lua/plugins/file_tree_plugins.lua
+++ lua/plugins/file_tree_plugins.lua
@@ -2,11 +2,16 @@ return {
 	{
 		"stevearc/oil.nvim",
 		opts = {},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+		dependencies = { "nvim-tree/nvim-web-devicons", "refractalize/oil-git-status.nvim" },
 		config = function()
 			require("oil").setup({
 				delete_to_trash = true,
+				win_options = {
+					signcolumn = "yes:2",
+				},
 			})
+
+			require("oil-git-status").setup()
 		end,
 		lazy = false,
 		keys = {
