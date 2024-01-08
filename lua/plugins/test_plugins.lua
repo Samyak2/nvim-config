4d41d5169bda18612b76665b0e99cf843af993d6
Samyak S Sarnayak <samyak201@gmail.com>
2024-01-09 00:21:27 +0530

    Add test plugins

---
+++ lua/plugins/test_plugins.lua
@@ -0,0 +1,77 @@
+return {
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/neotest-python",
+			"adrigzr/neotest-mocha",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-python"),
+					require("neotest-mocha"),
+				},
+			})
+		end,
+		cmd = { "Neotest" },
+		keys = {
+			{
+				"<leader>tt",
+				function()
+					require("neotest").run.run()
+				end,
+			},
+			{
+				"<leader>ts",
+				function()
+					require("neotest").run.stop()
+				end,
+			},
+			{
+				"<leader>tf",
+				function()
+					require("neotest").run.run(vim.fn.expand("%"))
+				end,
+			},
+			{
+				"<leader>ta",
+				function()
+					require("neotest").run.attach()
+				end,
+			},
+			{
+				"<leader>to",
+				function()
+					require("neotest").output.open({ enter = true })
+				end,
+			},
+			{
+				"[n",
+				function()
+					require("neotest").jump.prev({ status = "failed" })
+				end,
+			},
+			{
+				"]n",
+				function()
+					require("neotest").jump.next({ status = "failed" })
+				end,
+			},
+			{
+				"[t",
+				function()
+					require("neotest").jump.prev()
+				end,
+			},
+			{
+				"]t",
+				function()
+					require("neotest").jump.next()
+				end,
+			},
+		},
+	},
+}
