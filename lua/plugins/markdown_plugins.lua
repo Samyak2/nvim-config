5c4f8cedc802c2c773ead46b2a2e63850d91378e
Samyak S Sarnayak <samyak201@gmail.com>
2024-01-15 23:40:13 +0530

    Fix markdown-preview.nvim config

--- lua/plugins/markdown_plugins.lua
+++ lua/plugins/markdown_plugins.lua
@@ -1,8 +1,11 @@
 return {
     {
         "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
         ft = { "markdown" },
-        cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
-        config = "vim.call('mkdp#util#install')",
     },
 }
