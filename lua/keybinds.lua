2fd9a8bc27f7658966644de7c2a9548dc22676ec
Samyak S Sarnayak <samyak201@gmail.com>
2024-02-12 22:17:20 +0530

    keybind to clear search

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -3,3 +3,6 @@ local default_opts = { silent = true }
 -- Move up/down lines even when a line has been wrapped
 vim.keymap.set("n", "j", "gj", default_opts)
 vim.keymap.set("n", "k", "gk", default_opts)
+
+-- clear search highlight
+vim.keymap.set("n", "<C-s>", "<cmd>nohlsearch<cr>", default_opts)
