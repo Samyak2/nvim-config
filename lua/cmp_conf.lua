7dad20625bccd395b6e0091dc469ae533d637b78
Samyak S Sarnayak <samyak201@gmail.com>
2024-02-12 22:11:46 +0530

    Fix cmp config and add signature help plugin

--- lua/cmp_conf.lua
+++ lua/cmp_conf.lua
@@ -21,14 +21,15 @@ cmp.setup {
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert {
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm { select = false }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     },
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = 'nvim_lsp_signature_help' },
         { name = "vsnip" }, -- For vsnip users.
         -- { name = 'luasnip' }, -- For luasnip users.
         -- { name = 'ultisnips' }, -- For ultisnips users.
@@ -67,7 +68,7 @@ cmp.setup.filetype("gitcommit", {
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline("/", {
+cmp.setup.cmdline({"/", "?"}, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
         { name = "buffer" },
--- lua/plugins/autocomplete_plugins.lua
+++ lua/plugins/autocomplete_plugins.lua
@@ -20,6 +20,12 @@ return {
                 "hrsh7th/cmp-nvim-lsp",
             },
             {
+                "hrsh7th/cmp-nvim-lsp-document-symbol",
+            },
+            {
+                "hrsh7th/cmp-nvim-lsp-signature-help",
+            },
+            {
                 "hrsh7th/cmp-vsnip",
             },
             {
