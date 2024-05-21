c4df97d42d0e7bd425434504af3eca5a874bde80
Samyak S Sarnayak <samyak201@gmail.com>
2024-05-21 23:32:11 +0530

    Enable inlay hints and add rustaceanvim code actions

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -23,12 +23,19 @@ vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, default_keymap_opts)
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
+    callback = function(args)
+        local bufnr = args.buf
+
         -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+        vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client ~= nil and client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+        end
 
         local keymap_opts = vim.tbl_deep_extend("force", default_keymap_opts, {
-            buffer = ev.buf,
+            buffer = bufnr,
         })
 
         -- Mappings.
--- lua/lsp/rust-ls.lua
+++ lua/lsp/rust-ls.lua
@@ -1,5 +1,12 @@
 vim.g.rustaceanvim = {
     server = vim.tbl_deep_extend("force", require("lsp").common_opts(), {
+        on_attach = function(client, bufnr)
+            require("lsp").common_on_attach(client, bufnr)
+
+            vim.keymap.set("n", "<leader>ca", function()
+                vim.cmd.RustLsp("codeAction")
+            end, { silent = true, buffer = bufnr })
+        end,
         default_settings = {
             -- rust-analyzer language server configuration
             ["rust-analyzer"] = {
