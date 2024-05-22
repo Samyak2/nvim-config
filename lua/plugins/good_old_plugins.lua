dbd50e2c1349f1e0c3b6e87c5f096e512da0602b
Samyak S Sarnayak <samyak201@gmail.com>
2024-05-23 00:22:31 +0530

    Only enable vim-commentary in neovim < 0.10

--- lua/plugins/good_old_plugins.lua
+++ lua/plugins/good_old_plugins.lua
@@ -1,17 +1,21 @@
+---@type LazySpec
 return {
-    -- TODO: not needed in neovim v0.10
     {
         "tpope/vim-commentary",
         keys = {
             { "gc", mode = { "x" } },
             { "gcc", mode = { "n" } },
         },
+        cond = function()
+            -- not needed in neovim 0.10+
+            return vim.fn.has("nvim-0.10") == 0
+        end,
     },
     {
         "tpope/vim-surround",
         keys = {
             { "cs" },
-        }
+        },
     },
     {
         "mattn/emmet-vim",
