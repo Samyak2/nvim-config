77abbcd1ad354ada3c85d99eb993a9f7ad685a85
Samyak S Sarnayak <samyak201@gmail.com>
2024-05-22 22:01:47 +0530

    Trying a new colorscheme - poimandres!

---
+++ lua/plugins/colorscheme_plugins.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+    {
+        "olivercederborg/poimandres.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("poimandres").setup {
+                disable_background = true,
+            }
+        end,
+        init = function()
+            vim.cmd("colorscheme poimandres")
+        end,
+    },
+}
