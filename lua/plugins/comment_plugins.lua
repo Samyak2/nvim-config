f5e174346b0cc6de6e422700117f883c87a14752
Samyak S Sarnayak <samyak201@gmail.com>
2024-05-23 11:33:36 +0530

    Switch to Comment.nvim

---
+++ lua/plugins/comment_plugins.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+    -- still better than built-in commenting in 0.10:
+    -- https://github.com/numToStr/Comment.nvim/issues/453
+    "numToStr/Comment.nvim",
+    opts = {},
+    lazy = false,
+}
--- lua/plugins/good_old_plugins.lua
+++ lua/plugins/good_old_plugins.lua
@@ -1,17 +1,6 @@
 ---@type LazySpec
 return {
     {
-        "tpope/vim-commentary",
-        keys = {
-            { "gc", mode = { "x" } },
-            { "gcc", mode = { "n" } },
-        },
-        cond = function()
-            -- not needed in neovim 0.10+
-            return vim.fn.has("nvim-0.10") == 0
-        end,
-    },
-    {
         "tpope/vim-surround",
         keys = {
             { "cs" },
