e4fe52e5f343526fb336feeea787c71c9d9569ba
Samyak S Sarnayak <samyak201@gmail.com>
2024-06-06 08:43:46 +0530

    Use npx to get yarn when installing markdown-preview.nvim

--- lua/plugins/markdown_plugins.lua
+++ lua/plugins/markdown_plugins.lua
@@ -2,7 +2,7 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
+        build = "cd app && npx --yes yarn install",
         init = function()
             vim.g.mkdp_filetypes = { "markdown" }
         end,
