b0b44427947aaa90eee8cbd5958014edd57dbbbc
Samyak S Sarnayak <samyak201@gmail.com>
2024-07-05 00:06:04 +0530

    Fix yamlls schemastore config

--- lua/lsps.lua
+++ lua/lsps.lua
@@ -25,6 +25,13 @@ require("mason-lspconfig").setup_handlers {
         require("lspconfig").yamlls.setup {
             settings = {
                 yaml = {
+                    schemaStore = {
+                        -- You must disable built-in schemaStore support if you want to use
+                        -- schemastore.nvim and its advanced options like `ignore`.
+                        enable = false,
+                        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                        url = "",
+                    },
                     schemas = require("schemastore").yaml.schemas(),
                 },
             },
