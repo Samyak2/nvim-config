0bdd800664209d43372a65cffa3a944e94e80e0c
Samyak S Sarnayak <samyak201@gmail.com>
2024-08-31 00:29:34 +0530

    Fix rust-analyzer hanging

--- lua/lsp/rust-ls.lua
+++ lua/lsp/rust-ls.lua
@@ -1,5 +1,12 @@
 vim.g.rustaceanvim = {
     server = vim.tbl_deep_extend("force", require("lsp").common_opts(), {
+        capabilities = {
+            workspace = {
+                -- https://github.com/hrsh7th/cmp-nvim-lsp/issues/44#issuecomment-2096368152
+                -- but also needed files.excludeDirs below
+                didChangeWatchedFiles = { dynamicRegistration = true },
+            },
+        },
         on_attach = function(client, bufnr)
             require("lsp").common_on_attach(client, bufnr)
 
@@ -12,6 +19,12 @@ vim.g.rustaceanvim = {
             ["rust-analyzer"] = {
                 check = {
                     command = "clippy",
+                },
+                files = {
+                    excludeDirs = {
+                        ".direnv",
+                        "target"
+                    },
                 },
             },
         },
