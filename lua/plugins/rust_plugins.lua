b956a5b79d3396269add36a8fc0b5f90e9e49f72
Samyak S Sarnayak <samyak201@gmail.com>
2024-10-21 20:27:32 +0530

    Update rustaceanvim to v5

--- lua/plugins/rust_plugins.lua
+++ lua/plugins/rust_plugins.lua
@@ -1,7 +1,7 @@
 return {
     {
         "mrcjkb/rustaceanvim",
-        version = "^4", -- Recommended
+        version = "^5", -- Recommended
         lazy = false, -- This plugin is already lazy
     },
 }
