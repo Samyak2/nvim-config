0718106c17db6814a095d2e01bb5a05575a98a1f
Samyak S Sarnayak <samyak201@gmail.com>
2025-06-08 16:08:34 +0530

    Fix Snacks error and integrate with flash.nvim

--- lua/plugins/movement_plugins.lua
+++ lua/plugins/movement_plugins.lua
@@ -48,6 +48,44 @@ return {
                 desc = "Toggle Flash Search",
             },
         },
+        specs = {
+            {
+                "folke/snacks.nvim",
+                opts = {
+                    picker = {
+                        win = {
+                            input = {
+                                keys = {
+                                    ["<c-s>"] = { "flash", mode = { "n", "i" } },
+                                    ["s"] = { "flash" },
+                                },
+                            },
+                        },
+                        actions = {
+                            flash = function(picker)
+                                require("flash").jump {
+                                    pattern = "^",
+                                    label = { after = { 0, 0 } },
+                                    search = {
+                                        mode = "search",
+                                        exclude = {
+                                            function(win)
+                                                return vim.bo[vim.api.nvim_win_get_buf(win)].filetype
+                                                    ~= "snacks_picker_list"
+                                            end,
+                                        },
+                                    },
+                                    action = function(match)
+                                        local idx = picker.list:row2idx(match.pos[1])
+                                        picker.list:_move(idx, true, true)
+                                    end,
+                                }
+                            end,
+                        },
+                    },
+                },
+            },
+        },
     },
     {
         "nacro90/numb.nvim",
